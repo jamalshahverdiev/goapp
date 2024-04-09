223eeee59de200609be816f418e90fc5b52cdceb
Jamal Shahverdiev <jamal.shahverdiev@gmail.com>
2024-04-09 11:58:43 +0400

    Add new files

--- go.mod
+++ go.mod
@@ -1,11 +1,22 @@
-module github.com/nuggien/devops_test
+module goapp
 
-go 1.16
+go 1.21
 
 require (
 	github.com/go-redis/redis/v8 v8.11.2
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/lib/pq v1.10.2
-	github.com/rs/cors v1.8.0
-	go.uber.org/zap v1.19.0
+	github.com/prometheus/client_golang v1.19.0
+	go.uber.org/zap v1.27.0
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,18 +3,55 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/julienschmidt/httprouter"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
 	"log"
 	"net/http"
 	"os"
-	"github.com/go-redis/redis/v8"
-	"github.com/julienschmidt/httprouter"
-	"go.uber.org/zap"
 	"time"
 )
 
 type server struct {
-	redis redis.UniversalClient
+	redis  redis.UniversalClient
 	logger *zap.Logger
+}
+
+var httpRequestsTotal = prometheus.NewCounterVec(
+    prometheus.CounterOpts{
+        Name: "http_requests_total",
+        Help: "Total number of HTTP requests by status code, method, and path.",
+    },
+    []string{"code", "method", "path"},
+)
+
+func init() {
+    prometheus.MustRegister(httpRequestsTotal)
+}
+
+type responseWriter struct {
+    http.ResponseWriter
+    statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+    rw.statusCode = code
+    rw.ResponseWriter.WriteHeader(code)
+}
+
+func newPrometheusMiddleware(next httprouter.Handle) httprouter.Handle {
+    return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+        wrappedWriter := &responseWriter{ResponseWriter: w, statusCode: 200} // Default to 200 unless set otherwise
+        next(wrappedWriter, r, ps)
+
+        httpRequestsTotal.With(prometheus.Labels{
+            "code":   fmt.Sprintf("%d", wrappedWriter.statusCode),
+            "method": r.Method,
+            "path":   r.URL.Path,
+        }).Inc()
+    }
 }
 
 func main() {
@@ -30,13 +67,14 @@ func main() {
 	})
 
 	srv := &server{
-		redis: rdb,
+		redis:  rdb,
 		logger: logger,
 	}
 
 	router := httprouter.New()
-
-	router.GET("/", srv.indexHandler)
+	router.GET("/", newPrometheusMiddleware(srv.indexHandler))
+	router.GET("/hello", newPrometheusMiddleware(srv.helloHandler))
+	router.Handler("GET", "/metrics", promhttp.Handler())
 
 	logger.Info("server started on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
@@ -47,12 +85,17 @@ func (s *server) indexHandler(w http.ResponseWriter, r *http.Request, _ httprout
 	var err error
 	if v, err = s.redis.Get(context.Background(), "updated_time").Result(); err != nil {
 		s.logger.Info("updated_time not found, setting it")
-		v = time.Now().Format("2006-01-02 03:04:05")
+		v = time.Now().Format("2006-01-02 15:04:05")
 		s.redis.Set(context.Background(), "updated_time", v, 5*time.Second)
 	} else {
 		s.logger.Info("got updated_time")
 	}
 
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "hello world: updated_time=%s\n", v)
+	fmt.Fprintf(w, "Hello world: updated_time=%s\n", v)
+}
+
+func (s *server) helloHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Hello world!")
 }
